bcb74fe1223c6e70eb2171da36c5830785ba3e0c
Jimmy Yao <jimmyyaocs@gmail.com>
2022-06-14 00:09:09 +0930

    Final version

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyCwcbi1Cw6j0kJiobYfWTSIIyPkOW5aheY",
+  authDomain: "blackscreen-85de5.firebaseapp.com",
+  projectId: "blackscreen-85de5",
+  storageBucket: "blackscreen-85de5.appspot.com",
+  messagingSenderId: "802887592429",
+  appId: "1:802887592429:web:13b5748002d2f130d01fe4",
 };
 
 // Initialize Firebase
